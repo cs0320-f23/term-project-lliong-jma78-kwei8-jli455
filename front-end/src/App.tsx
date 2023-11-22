415cdc6fd95bbeda30d915393292e8b4d9a0fbc2
Kelly Wei <kelly_wei@brown.edu>
2023-11-22 12:02:37 -0500

    chore: skeleton code of multiple pages

---
+++ front-end/src/App.tsx
@@ -0,0 +1,23 @@
+import "./styles/App.css";
+import React from "react";
+import Navbar from "./components/NavBarElements";
+
+import{
+  BrowserRouter as Router, Routes, Route, 
+} from "react-router-dom"
+
+import Home from "./pages/home";
+import About from "./pages/about"
+
+
+/**
+ * This is the main class that runs our app.
+ * @returns 
+ */
+function App() {
+  return <div className="App">
+
+  </div>
+}
+
+export default App;
