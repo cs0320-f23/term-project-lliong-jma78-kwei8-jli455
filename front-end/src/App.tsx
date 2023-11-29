c5b6be14f745cff545b535718fbde378245bb5c4
jli02 <113394184+jli02@users.noreply.github.com>
2023-11-29 17:12:16 -0500

    moved index.html

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,22 +2,22 @@ import "./styles/App.css";
 import React from "react";
 import Navbar from "./components/NavBarElements";
 
-import{
-  BrowserRouter as Router, Routes, Route, 
-} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home";
-import About from "./pages/about"
-
+//import Home from "./pages/home";
+//import About from "./pages/about";
+//import Artists from ".pages/explore_artists";
 
 /**
  * This is the main class that runs our app.
- * @returns 
+ * @returns
  */
 function App() {
-  return <div className="App">
-
-  </div>
+  return (
+    <div className="App">
+      <h1>hi</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ front-end/src/pages/explore_artists.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//  maybe need file for each artist and use this file as main component to hold them?
+export function Artists() {
+  return (
+    <div>
+      <button>Submit</button>
+    </div>
+  );
+}
