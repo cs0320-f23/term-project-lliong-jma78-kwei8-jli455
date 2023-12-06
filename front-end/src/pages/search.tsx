6e25d98bc9ec2af9145776a77316cc312a6f5e15
jli02 <113394184+jli02@users.noreply.github.com>
2023-12-06 02:53:32 -0500

    added area for potential error messages

--- front-end/src/pages/search.tsx
+++ front-end/src/pages/search.tsx
@@ -5,7 +5,9 @@ import { Searchbar, NumberInput } from "./search_bar";
 export function Search() {
   const [commandString, setCommandString] = useState<string>("");
   const [numCommandString, setNumCommandString] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
+  // add errors - where to display - below the input boxes
   function handleSubmit() {
     setCommandString("");
     console.log("button clicked");
@@ -27,7 +29,10 @@ export function Search() {
           setNumValue={setNumCommandString}
           ariaLabel={"number search bar"}
         />
+        <br></br>
+        {message}
       </div>
+
       <button
         className="submit-button"
         aria-label="submit button"
