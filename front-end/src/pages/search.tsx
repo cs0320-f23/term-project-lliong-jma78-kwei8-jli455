62a2ba502d2030d47eb00421ff46ce2cf2f77616
jli02 <113394184+jli02@users.noreply.github.com>
2023-12-05 23:51:00 -0500

    feat: updated submit css

--- front-end/src/pages/search.tsx
+++ front-end/src/pages/search.tsx
@@ -5,6 +5,12 @@ import { Searchbar } from "./search_bar";
 export function Search() {
   const [commandString, setCommandString] = useState<string>("");
 
+  function handleSubmit(commandString: string) {
+    setCommandString("");
+    console.log("button clicked");
+    console.log(commandString);
+  }
+
   return (
     <div className="searchbar-container">
       <Searchbar
@@ -12,7 +18,11 @@ export function Search() {
         setValue={setCommandString}
         ariaLabel={"search bar"}
       />
-      <button className="submit-button" aria-label="submit button">
+      <button
+        className="submit-button"
+        aria-label="submit button"
+        onClick={() => handleSubmit(commandString)}
+      >
         Submit
       </button>
     </div>
