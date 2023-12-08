fc69eb354aa6df4573d57b67ae9e422b09171fa7
Kelly Wei <kelly_wei@brown.edu>
2023-12-08 17:48:16 -0500

    mapbox added

--- front-end/src/components/MapBox.tsx
+++ front-end/src/components/MapBox.tsx
@@ -1,77 +1,34 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useState } from "react";
 import Map, {Layer, MapLayerMouseEvent, Source, ViewStateChangeEvent} from "react-map-gl";
-
 import "mapbox-gl/dist/mapbox-gl.css"
-
-import { APIKey } from "../private/key";
-// import { geoLayer, overlayData } from "./overlay";
+import { APIKey } from "../../private/key";
 
 export default function MapBox(){
-    const pvdLatitude = 41.82780
-    const pvdLongitude = -71.40125
-    const initialZoom = 20
+  const pvdLatitude = 41.82780
+  const pvdLongitude = -71.40125
+  const initialZoom = 10
 
+  const[viewState, setViewState] = useState({
+    longitude: pvdLongitude,
+    latitude: pvdLatitude,
+    zoom: initialZoom
+  })
 
-    const[viewState, setViewState] = useState({
-        longitude: pvdLongitude,
-        latitude: pvdLatitude,
-        zoom: initialZoom
-    })
+  return(
+    <Map
+      mapboxAccessToken={APIKey}
+      longitude={viewState.longitude}
+      latitude={viewState.latitude}
+      zoom={viewState.zoom}
 
-    const [pointData, setPointData] = useState({
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [-71.41254,41.82429],
-            },
-            properties: {
-              title: 'Providence',
-            },
-          },
-          // Add more points as needed
-        ],
-      });
+      onMove={(ev: ViewStateChangeEvent) => setViewState(ev.viewState)}
+      style={{width: window.innerWidth, height: window.innerHeight}}
+      mapStyle={"mapbox://styles/mapbox/outdoors-v12"}
+    
+    >
 
-    const[overlay, setOverlay] = useState<GeoJSON.FeatureCollection | undefined>(undefined);
+    </Map>
+  )
 
-    // useEffect(() => {
-    //     setOverlay(overlayData())
-    // }, []);
-
-    function onMapClick(e: MapLayerMouseEvent){
-        console.log(e.lngLat.lat);
-        console.log(e.lngLat.lng)
-    }
-
-
-
-    //returning the map component 
-    return(
-        <Map 
-        
-        mapboxAccessToken={APIKey}
-        longitude={viewState.longitude}
-        latitude={viewState.latitude}
-        zoom={viewState.zoom}
-
-        onMove={(ev: ViewStateChangeEvent) => setViewState(ev.viewState)}
-
-        style={{width: window.innerWidth, height:window.innerHeight}}
-
-        mapStyle={"mapbox://styles/mapbox/outdoors-v12"}
-
-        onClick={(ev:MapLayerMouseEvent) => onMapClick(ev)}
-        
-        >
-            {/* <Source id="geo_data" type="geojson" data={overlayData()}>            
-                <Layer {...geoLayer}></Layer>
-            </Source    > */}
-
-            {/* <Source id="income_highlight" type="geojson" data={pointData}></Source> */}
-        </Map>
-    )
 }
+
