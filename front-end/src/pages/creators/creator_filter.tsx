017969b6d20991b8996360ef58e35f65f677689e
jli02 <113394184+jli02@users.noreply.github.com>
2023-12-08 12:28:36 -0500

    implementing filter buttons

---
+++ front-end/src/pages/creators/creator_filter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function CreatorFilterButtons() {
+  return (
+    <div className="creator-filters">
+      <button>Visual Arts</button>
+      <button>Performing Arts</button>
+      <button>Literary Arts</button>
+      <button>Artisans</button>
+      <button>Other</button>
+    </div>
+  );
+}
--- front-end/src/pages/creators/creator_search.tsx
+++ front-end/src/pages/creators/creator_search.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { Search } from "react-router-dom";
 import { Searchbar } from "../creators/creator_search_bar";
+import { CreatorFilterButtons } from "./creator_filter";
 
 export function Search() {
   const [commandString, setCommandString] = useState<string>("");
@@ -18,23 +19,28 @@ export function Search() {
   }
 
   return (
-    <div className="creator-searchbar-container" onKeyDown={keyDownHandler}>
-      <div>
-        <Searchbar
-          value={commandString}
-          setValue={setCommandString}
-          ariaLabel={"creator name and description search bar"}
-        />
-        <br></br>
-        {message}
+    <div>
+      <div className="creator-searchbar-container" onKeyDown={keyDownHandler}>
+        <div>
+          <Searchbar
+            value={commandString}
+            setValue={setCommandString}
+            ariaLabel={"creator name and description search bar"}
+          />
+          <br></br>
+          {message}
+        </div>
+        <button
+          className="submit-button"
+          aria-label="submit button"
+          onClick={() => handleSubmit()}
+        >
+          Submit
+        </button>
       </div>
-      <button
-        className="submit-button"
-        aria-label="submit button"
-        onClick={() => handleSubmit()}
-      >
-        Submit
-      </button>
+      <div className="creator-filters">
+        <CreatorFilterButtons />
+      </div>
     </div>
   );
 }
