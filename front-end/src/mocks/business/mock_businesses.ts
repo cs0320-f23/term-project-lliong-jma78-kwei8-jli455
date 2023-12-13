78831ad24fd0a996ef5d413f4f2e2b88f2786853
Kelly Wei <kelly_wei@brown.edu>
2023-12-12 22:28:54 -0500

    chore: created mocked values of college hill asian-owned businesses

---
+++ front-end/src/mocks/business/mock_businesses.ts
@@ -0,0 +1,388 @@
+const business_dataset = [
+    {
+        'name': 'Heng Thai & Rotisserie',
+        'url': 'https://www.hengpvd.com/home',
+        'review_count': 346,
+        'business':'restaurants',
+        'categories':[{
+            'alias':'thai',
+            'title':'Thai',
+        }],
+        'rating': 4.6,
+        'latitude': 41.82784,
+        'longitude': 71.40122,
+        'transactions':['pickup'],
+        'price': '$$',
+        'location':{
+            'address1': '165 Angell St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+14017511651'
+    },
+    {
+        'name': 'JLife Mart',
+        'url': 'https://www.instagram.com/jlife_mart/?hl=en',
+        'review_count': 28,
+        'business':'groceries',
+        'categories':[
+            {
+                'alias':'grocery',
+                'title':'Grocery',
+            },
+            {
+                'alias': 'markets',
+                'title': 'Snacks & Beauty'
+            },
+        ],
+        'rating': 3.9,
+        'latitude': 41.82803,
+        'longitude': 71.40046,
+        'transactions':[],
+        'price': '$',
+        'location':{
+            'address1': '231 Thayer St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+14017511651'
+    },
+    {
+        'name': 'Ceremony',
+        'url': 'https://www.ceremonypvd.com/',
+        'review_count': 226,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'cafe',
+                'title':'Cafe',
+            },
+            {
+                'alias': 'markets',
+                'title': 'Snacks'
+            },
+        ],
+        'rating': 4.6,
+        'latitude': 41.82860,
+        'longitude': 71.39977,
+        'transactions':[],
+        'price': '$',
+        'location':{
+            'address1': '406 Brook St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':''        
+    },
+    {
+        'name': 'bb.q Chicken + Soban Korean Eatery',
+        'url': 'https://www.sobanbbqchicken.com/',
+        'review_count': 362,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'korean',
+                'title':'Korean',
+            },
+        ],
+        'rating': 4.0,
+        'latitude': 41.82925,
+        'longitude': 71.40092,
+        'transactions':['pickup'],
+        'price': '$$',
+        'location':{
+            'address1': '272 Thayer St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+14017511234'  
+    },
+    {
+        'name': 'Den Den Korean Fried Chicken',
+        'url': 'https://www.dendenhospitality.com/dendenfriedchicken',
+        'review_count': 1236,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'korean',
+                'title':'Korean',
+            },
+        ],
+        'rating': 4.6,
+        'latitude': 41.82831,
+        'longitude': 71.40031,
+        'transactions':['pickup'],
+        'price': '$$',
+        'location':{
+            'address1': '182 Angell St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+14012349955'
+    },
+    {
+        'name': 'Tiger Sugar',
+        'url': 'https://www.facebook.com/TigerSugarProvidence/',
+        'review_count': 56,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'boba shop',
+                'title':'Boba Shop',
+            },
+        ],
+        'rating': 4.2,
+        'latitude': 41.82986,
+        'longitude': 71.40105,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '288 Thayer St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4018544179'  
+    },
+    {
+        'name': 'Vivi Bubble Tea',
+        'url': 'https://vivi-bubble-tea-ri.business.site/',
+        'review_count': 208,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'boba shop',
+                'title':'Boba Shop',
+            },
+        ],
+        'rating': 4.1,
+        'latitude': 41.82779,
+        'longitude': 71.40074,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '224 Thayer St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4012728888'  
+    },
+    {
+        'name': 'Mala Noodles',
+        'url': 'http://www.malanoodles.com/',
+        'review_count': 43,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'chinese',
+                'title':'Chinese',
+            },
+        ],
+        'rating': 4.2,
+        'latitude': 41.82754,
+        'longitude': 71.40033,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '215 Thayer St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4012859999'  
+    },
+    {
+        'name': "Wong's Kitchen",
+        'url': 'https://orderwongstogo.com/',
+        'review_count': 207,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'vietnamese',
+                'title':'Vietnamese',
+            },
+            {
+                'alias':'chinese',
+                'title':'Chinese',
+            },
+        ],
+        'rating': 4.1,
+        'latitude': 41.82752,
+        'longitude': 71.40010,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '108 Waterman St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4012859999'  
+    },
+    {
+        'name': "Kung Fu Tea",
+        'url': 'https://menu.kungfutea.com/mobile/65',
+        'review_count': 479,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'boba shop',
+                'title':'Boba Shop',
+            },
+        ],
+        'rating': 4.4,
+        'latitude': 41.82752,
+        'longitude': 71.39994,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '110 Waterman St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4018611888'  
+    },
+    {
+        'name': "DaDaruki Express",
+        'url': 'https://www.doordash.com/store/dadaruki-express-providence-26110461/',
+        'review_count': 21,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'japanese',
+                'title':'Japanese',
+            },
+        ],
+        'rating': 4.3,
+        'latitude': 41.82752,
+        'longitude': 71.39975,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '112 Waterman St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4014210754'  
+    },
+    {
+        'name': "Ten One Tea House",
+        'url': 'https://www.tenoneteahouse.com/',
+        'review_count': 135,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'boba shop',
+                'title':'Boba Shop',
+            },
+        ],
+        'rating': 4.2,
+        'latitude': 41.82744,
+        'longitude': 71.40073,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '216 Thayer St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4016421516'  
+    },
+    {
+        'name': "Kabob and Curry",
+        'url': 'https://kabobandcurry.com/',
+        'review_count': 983,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'indian',
+                'title':'Indian',
+            },
+        ],
+        'rating': 4.4,
+        'latitude': 41.82892,
+        'longitude': 71.40052,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '261 Thayer St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4012738844'  
+    },
+    {
+        'name': "Yan's Cuisine of Providence",
+        'url': 'https://yans-cuisine-of-providence-inc.square.site/',
+        'review_count': 183,
+        'business':'restaurants',
+        'categories':[
+            {
+                'alias':'chinese',
+                'title':'Chinese',
+            },
+        ],
+        'rating': 3.7,
+        'latitude': 41.82476,
+        'longitude': 71.39906,
+        'transactions':['pickup'],
+        'price': '$',
+        'location':{
+            'address1': '83 Benevolent St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4017518888'  
+    },
+    {
+        'name': "Smart Cuts",
+        'url': 'https://www.facebook.com/smartcutsri/',
+        'review_count': 183,
+        'business':'services',
+        'categories':[
+            {
+                'alias':'hair salon',
+                'title':'Hair Salon',
+            },
+        ],
+        'rating': 4.7,
+        'latitude': 41.82453,
+        'longitude': 71.39913,
+        'transactions':[],
+        'price': '$',
+        'location':{
+            'address1': '284 Brook St',
+            'address2':'',
+            'city': 'Providence',
+            'country': 'US',
+            'state': 'RI'
+        },
+        'phone':'+4012434370'  
+    },
+    
+]
