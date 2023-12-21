786504d4c1e0789f2789923b94050e4d0a507818
Kelly Wei <kelly_wei@brown.edu>
2023-12-21 14:31:36 -0500

    chore: updated css

--- front-end/src/pages/home.tsx
+++ front-end/src/pages/home.tsx
@@ -3,20 +3,21 @@ import "../styles/home.css";
 import logoPlaceholder from '../images/nugget-dino3.png';
 import downArrow from '../images/down-arrow.png';
 import { useNavigate } from "react-router-dom";
+import logo from "../images/ablaze_logo.png"
 
 export default function Home() {
   // const navigate = useNavigate();
 
   return(
-    <div className="home-page">
-      <img src={logoPlaceholder} style={{paddingLeft:"42vw"}}></img>
+    <div className="home-page" style={{background:"#FCADAD"}}>
+      <img src={logo} style={{display:"flex", width:"10vw", height:"20vh", paddingLeft:"45vw", paddingTop:"20vh"}}></img>
       <h1 style={{fontSize:"100px"}}>Ablaze</h1>
-      <h2 style={{textAlign:"center"}}>Support Asian-Owned Businesses and Artists</h2>
+      <h2 style={{textAlign:"center"}}>Discover and Support Asian-Owned Businesses and Artists</h2>
       <div style={{paddingTop:"20vh"}}>
-        <h2 style={{textAlign:"center"}}>See Featured</h2>
+        {/* <h2 style={{textAlign:"center"}}>See Featured</h2>
         <img src={downArrow} 
             style={{width:"45px", height:"45px", paddingLeft:"48.5vw"}}
-        ></img>
+        ></img> */}
       </div>
     </div>
   )
