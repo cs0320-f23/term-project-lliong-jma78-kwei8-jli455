e4b0c9b6be85086640634430befad1deb689f2b6
Kelly Wei <kelly_wei@brown.edu>
2023-12-21 13:43:34 -0500

    merged from businesses

--- front-end/src/components/DropDown.tsx
+++ front-end/src/components/DropDown.tsx
@@ -25,13 +25,6 @@ export default function DropDownNav(){
     return(
         <div id="nav-bar">
             <nav className="nav-container">
-                <header id="header-row" aria-label="dropdown navigation bar" style={{float:"right"}}> 
-                    <div className="nav-pages" style={{}}>
-                        <img src={logo} style={{width: "90px", height:"80px", paddingInline:"50px"}} onClick={() => navigate("/home")}></img>
-                    </div>
-                    <div className="nav-pages" style={{float:"left", paddingRight:"50vw"}}>
-                        <h1 onClick={() => navigate("/home")} style={{fontSize:"40px"}}>Ablaze</h1>
-                    </div>   
                 <header id="header-row" aria-label="dropdown navigation bar">
                     <div className="logo">
                         <Link to="/">
@@ -44,7 +37,7 @@ export default function DropDownNav(){
                         </Link>
                     </div>
                     <div className="nav-pages" onMouseEnter={handleHoverExplore} onMouseLeave={handleMouseLeave}>
-                        <h2 style={{paddingTop:"25px"}}>Explore</h2>
+                        <h2>Explore</h2>
                         {showExplore ? 
                             <div>
                                 <div className="dropdown-menu">
@@ -60,7 +53,7 @@ export default function DropDownNav(){
                             </div> : null}
                     </div>
                     <div className="nav-pages" onMouseEnter={handleHoverSubmit} onMouseLeave={handleMouseLeave}>
-                        <h2 style={{paddingTop:"25px"}}>Submit</h2>
+                        <h2>Submit</h2>
                         {showSubmit ? 
                             <div>
                                 <div className="dropdown-menu">
@@ -72,18 +65,12 @@ export default function DropDownNav(){
                             </div>
                         : null}
                     </div>
-
                     <div className="nav-pages">
-                        <h2 style={{paddingTop:"25px"}} onClick={() => navigate("/resources")}>Resources</h2>
-                        <h2><Link to="/resources">Resources</Link></h2>
+                        <h2 style={{color:"#4f100a"}}><Link to="/resources" style={{color:"#4f100a"}}>Resources</Link></h2>
                     </div>
                     <div className="nav-pages">
-                        <h2 style={{paddingTop:"25px"}} onClick={() => navigate("/about")}>About</h2>
+                        <img src={accountLogo} className="acc-logo"></img>
                     </div>
-                    <div className="nav-pages">
-                        <img src={accountLogo} style={{paddingTop:"25px"}} className="acc-logo"></img>
-                    </div>
-
                 </header>
             </nav>
             <Outlet />
